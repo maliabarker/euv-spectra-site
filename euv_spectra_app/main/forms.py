d3f72d682ad7c796ba32800ff27be05cae81251b
maliabarker <89502882+maliabarker@users.noreply.github.com>
2022-09-02 12:46:04 -1000

    Form submission functionality completed

--- euv_spectra_app/main/forms.py
+++ euv_spectra_app/main/forms.py
@@ -1,5 +1,6 @@
+from typing import Optional
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, SubmitField, IntegerField
+from wtforms import StringField, SelectField, SubmitField, IntegerField, validators
 from wtforms.validators import DataRequired, Length
 
 class StarForm(FlaskForm):
@@ -9,10 +10,11 @@ class StarForm(FlaskForm):
     stell_rad = IntegerField('R ☉ — Stellar Radius in Solar Radian', validators=[DataRequired()])
     dist = IntegerField('d — Distance', validators=[DataRequired()])
     dist_unit = SelectField('d Unit', validators=[DataRequired()], choices=[('pc', 'Parsecs (pc)'), ('mas', 'Milliarcseconds (mas)')])
-    fuv = IntegerField('FUV — Far Ultraviolet Spectra', validators=[DataRequired()])
+    fuv = IntegerField('FUV — Far Ultraviolet Spectra in Microjanskys (mjy)', validators=[validators.Optional()])
     fuv_flag = SelectField('FUV Flag', validators=[DataRequired()], choices=[('none', 'None'), ('null', 'Null'), ('upper_limit', 'Upper Limit')])
-    nuv = IntegerField('NUV — Near Ultraviolet Spectra', validators=[DataRequired()])
+    nuv = IntegerField('NUV — Near Ultraviolet Spectra in Microjanskys (mjy)', validators=[validators.Optional()])
     nuv_flag = SelectField('NUV Flag', validators=[DataRequired()], choices=[('none', 'None'), ('null', 'Null'), ('upper_limit', 'Upper Limit')])
+    submit = SubmitField('Next →')
 
 class StarNameForm(FlaskForm):
     name = StringField('Star Name', validators=[DataRequired()])
--- euv_spectra_app/main/routes.py
+++ euv_spectra_app/main/routes.py
@@ -8,7 +8,7 @@ from euv_spectra_app.main.forms import StarForm, StarNameForm
 # FOR ASTROQUERY/GALEX DATA
 from astroquery.mast import Mast, Observations
 
-obs_table = Observations.query_object("Proxima Cen",radius=".02 deg")
+obs_table = Observations.query_object("2MASS 11315526-3436272",radius=".02 deg")
 
 import requests
 from urllib.parse import quote as urlencode
@@ -34,30 +34,43 @@ from euv_spectra_app.extensions import app, db
 
 main = Blueprint("main", __name__)
 
+'''
+TODO
+
+style form error better
+if there is a form error, keep the fuv and nuv inputs visible
+add flashes if there are form errors
+
+IDEA
+put fluxes as optional (meaning fields are empty) and check to see if there is any data for fluxes through API
+'''
+
 @main.route('/', methods=['GET', 'POST'])
 def homepage():
     # print(obs_table[:10][0])
     form = StarForm()
     name_form = StarNameForm()
+
     if request.method == 'POST':
         print('————————POSTING————————')
+
         if form.validate_on_submit():
             print('form validated!')
             print(form)
 
-            for item in form:
-                print(item)
-                print(form.item.data)
+            for fieldname, value in form.data.items():
+                print(fieldname, value)
             
-            return render_template('result.html')
+            return redirect(url_for('main.ex_result', formname='main'))
 
         elif name_form.validate_on_submit():
             print('name form validated!')
             print(name_form)
             
-            for item in name_form:
-                print(item)
-                print(name_form.item.data)
+            for fieldname, value in name_form.data.items():
+                print(fieldname, value)
+
+            return redirect(url_for('main.ex_result', formname='name'))
 
     return render_template('home.html', form=form, name_form=name_form)
 
