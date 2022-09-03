969b3b7a851ffaff9036a5b9d47a8343ec233015
maliabarker <89502882+maliabarker@users.noreply.github.com>
2022-09-02 14:31:37 -1000

    [add] found flux values through MAST db when searching by star name

--- euv_spectra_app/main/routes.py
+++ euv_spectra_app/main/routes.py
@@ -4,21 +4,20 @@ from datetime import date, datetime
 from euv_spectra_app.models import Star
 from euv_spectra_app.main.forms import StarForm, StarNameForm
 
-'''
+
 # FOR ASTROQUERY/GALEX DATA
-from astroquery.mast import Mast, Observations
+from astroquery.mast import Mast, Catalogs
 
-obs_table = Observations.query_object("2MASS 11315526-3436272",radius=".02 deg")
 
-import requests
-from urllib.parse import quote as urlencode
+# import requests
+# from urllib.parse import quote as urlencode
 
-from astropy.table import Table
-import numpy as np
+# from astropy.table import Table
+# import numpy as np
 
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
-'''
+
 
 
 # FOR PAGINATION OF RESULTS OR SEARCH
@@ -47,7 +46,7 @@ put fluxes as optional (meaning fields are empty) and check to see if there is a
 
 @main.route('/', methods=['GET', 'POST'])
 def homepage():
-    # print(obs_table[:10][0])
+    
     form = StarForm()
     name_form = StarNameForm()
 
@@ -60,15 +59,30 @@ def homepage():
 
             for fieldname, value in form.data.items():
                 print(fieldname, value)
+
+            '''QUESTIONS:
+                how are we going to search galex DB without a name?
+            '''
             
             return redirect(url_for('main.ex_result', formname='main'))
 
         elif name_form.validate_on_submit():
             print('name form validated!')
-            print(name_form)
+
+            star_name = name_form.name.data
+            print(star_name)
+
+            '''GETTING FLUX VALUES FROM GALEX'''
+            catalog_data = Catalogs.query_object(name_form.name.data, radius=.02, catalog="GALEX")
+            MIN_DIST = catalog_data['distance_arcmin'] < 0.1
             
-            for fieldname, value in name_form.data.items():
-                print(fieldname, value)
+            filtered_data = catalog_data[MIN_DIST]
+            filtered_data.pprint()
+
+            fuv = filtered_data['fuv_flux']
+            nuv = filtered_data['nuv_flux']
+
+            print(f'FLUXES {fuv}, {nuv}')
 
             return redirect(url_for('main.ex_result', formname='name'))
 
