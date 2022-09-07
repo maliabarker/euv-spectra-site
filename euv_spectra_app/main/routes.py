7ba8a70e6a72b6ed7fae2b46d104b602349453f8
maliabarker <89502882+maliabarker@users.noreply.github.com>
2022-09-07 13:28:32 -1000

    [edit] turned galex search code into helper function

--- euv_spectra_app/main/routes.py
+++ euv_spectra_app/main/routes.py
@@ -1,23 +1,32 @@
 from flask import Blueprint, request, render_template, redirect, url_for
-from datetime import date, datetime
 
 from euv_spectra_app.models import Star
 from euv_spectra_app.main.forms import StarForm, StarNameForm
 
 # FOR ASTROQUERY/GALEX DATA
-from astroquery.mast import Mast, Catalogs
-from astroquery.ipac.nexsci.nasa_exoplanet_archive import NasaExoplanetArchive
-from astroquery.simbad import Simbad
-from astropy.coordinates import SkyCoord
+from astroquery.mast import Catalogs
 import astropy.units as u
 from astroquery.vizier import Vizier
-
-from re import search
-
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
+
 main = Blueprint("main", __name__)
+
+
+# ——————————— HELPER FUNCTIONS ————————————— #
+def search_galex(search_input):
+    flux_catalog_data = Catalogs.query_object(search_input, radius=.02, catalog="GALEX")
+    MIN_DIST = flux_catalog_data['distance_arcmin'] < 0.1
+    filtered_data = flux_catalog_data[MIN_DIST][0]
+    fluxes = (filtered_data['fuv_flux'], filtered_data['nuv_flux'])
+    return fluxes
+
+    # print(filtered_data)
+    # ra = filtered_data['ra']
+    # dec = filtered_data['dec']
+    # print(f'Position {ra}, {dec}')
+
 
 '''
 ————TODO—————
@@ -28,18 +37,6 @@ main = Blueprint("main", __name__)
     outline incorrect form inputs in red? (stretch)
 
 2. query GALEX db when not searching by name
-
-3. create GALEX search function instead of direct code under form submission
-        # def search_galex_db():
-
-4. change front end for fluxes from buttons to simply optional inputs
-
-IDEAS
-put fluxes as optional (meaning fields are empty) and check to see if there is any data for fluxes through API
-
-QUESTIONS
-how are we going to search galex DB without a name? (ex: when searching with first form)
-what should we put as radius (also don't have to put radius)
 '''
 
 @main.route('/', methods=['GET', 'POST'])
@@ -61,29 +58,18 @@ def homepage():
             
             return redirect(url_for('main.ex_result', formname='main'))
 
+
         elif name_form.validate_on_submit():
 
             star_name = name_form.name.data
             print(f'name form validated with star: {star_name}')
 
             '''GETTING FLUX VALUES FROM GALEX'''
-            flux_catalog_data = Catalogs.query_object(name_form.name.data, radius=.02, catalog="GALEX")
-            MIN_DIST = flux_catalog_data['distance_arcmin'] < 0.1
-            filtered_data = flux_catalog_data[MIN_DIST][0]
-
-            # print(filtered_data)
-            # ra = filtered_data['ra']
-            # dec = filtered_data['dec']
-            # print(f'Position {ra}, {dec}')
-
-            fuv = filtered_data['fuv_flux']
-            nuv = filtered_data['nuv_flux']
+            fluxes = search_galex(star_name)
+            fuv, nuv = fluxes[0], fluxes[1]
             print(f'FLUXES {fuv}, {nuv}')
 
-            '''———GETTING OTHER VALUES———'''
-
-            print('————————————————————————————————————————————————')
-
+            '''GETTING OTHER STELLAR VALUES'''
             result = Vizier.query_region(star_name, radius=0.1*u.deg)
             tic82_table = result['IV/39/tic82'][0]
 
@@ -92,9 +78,9 @@ def homepage():
             mass = tic82_table['Mass']
             rad = tic82_table['Rad']
             dist = tic82_table['Dist']
-
             print(f'INFO Teff:{teff}, Logg:{logg}, Mass:{mass}, Rad:{rad}, Dist:{dist}')
 
+            
             catalog_data = Catalogs.query_object(star_name, radius=.02, catalog="TIC")
             table2 = catalog_data[0]
 
@@ -104,7 +90,6 @@ def homepage():
             rad2 = table2['rad']
             dist2A = table2['d']
             dist2B = table2['dstArcSec']
-
             print(f'INFO2 Teff:{teff2}, Logg:{logg2}, Mass:{mass2}, Rad:{rad2}, DistA:{dist2A}, DistB:{dist2B}')
             
             return redirect(url_for('main.ex_result', formname='name'))
