3ec82d4c0288a447514752f2b74fcff9e46208ab
maliabarker <89502882+maliabarker@users.noreply.github.com>
2022-10-12 16:09:25 -1000

    [add, delete] Added collections and helper functions to upload data from CSV files to mongodb atlas cluster and deleted old helper functions in util folder

--- euv_spectra_app/extensions.py
+++ euv_spectra_app/extensions.py
@@ -18,10 +18,22 @@ db = client.get_database(my_db)
 # ===========================
 
 # ======= Collections ==========
-# events = db.events
-# descriptions = db.descriptions
-# 
+model_parameter_grid = db.model_parameter_grid
+m0_grid = db.m0_grid
+m1_grid = db.m1_grid
+m2_grid = db.m2_grid
+m3_grid = db.m3_grid
+m4_grid = db.m4_grid
+m5_grid = db.m5_grid
+m6_grid = db.m6_grid
+m7_grid = db.m7_grid
+m8_grid = db.m8_grid
 # =========================
 
 # db=SQLAlchemy(app)
 # migrate = Migrate(app, db)
+
+
+
+# read_table('/Users/maliabarker/Desktop/NASA/EUV_Spectra_Site/euv_spectra_app/static/tables/M0_models.csv')
+# read_model_parameter_table('/Users/maliabarker/Desktop/NASA/EUV_Spectra_Site/euv_spectra_app/static/tables/model_parameter_grid.csv')
