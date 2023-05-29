82e9eeaa5b16036a7e348b7d453f856c9027f500
maliabarker <89502882+maliabarker@users.noreply.github.com>
2023-05-29 18:39:07 -0400

    [Edit] Added debouncing to star name input sesame resolver function so popovers wouldn't spam as a user types

--- euv_spectra_app/static/js/sesame_resolver.js
+++ euv_spectra_app/static/js/sesame_resolver.js
@@ -1,6 +1,7 @@
 const nameInput = document.querySelector('#name-form-input-group');
 const searchBtn = document.querySelector('#name-form-submit');
 const searchNameInput = document.querySelector('#name-form-input-group');
+let timeoutId;  // Variable to store the timeout ID
 nameInput.addEventListener('input', checkName);
 
 function runPopovers() {
@@ -12,24 +13,33 @@ function runPopovers() {
     });
 }
 
-
 function checkName() {
     const name = nameInput.value;
     popover = bootstrap.Popover.getInstance(searchNameInput);
     if (popover) {
-        popover.dispose()
-    } 
-    if (searchNameInput.value.length !== 0) {
-        searchNameInput.setAttribute('data-bs-toggle', 'popover');
-        searchNameInput.setAttribute('data-bs-trigger', 'manual');
-        searchNameInput.setAttribute('data-bs-placement', 'top');
-        searchNameInput.setAttribute('data-bs-content', 'Loading...')
-    } else {
-        popover.dispose();
+      popover.dispose();
     }
-    fetch(`https://cds.unistra.fr/cgi-bin/nph-sesame/-oIx/~S?${name}`)
-        .then(response => response.text())
+  
+    clearTimeout(timeoutId);
+  
+    if (searchNameInput.value.length !== 0) {
+      searchNameInput.setAttribute('data-bs-toggle', 'popover');
+      searchNameInput.setAttribute('data-bs-trigger', 'manual');
+      searchNameInput.setAttribute('data-bs-placement', 'top');
+      searchNameInput.setAttribute('data-bs-content', 'Loading...');
+  
+      // Show the popover immediately
+      searchNameInput.dispatchEvent(new Event('mouseenter'));
+    } else {
+      popover.dispose();
+    }
+  
+    // Set a new timeout to execute the function after 2 seconds of inactivity
+    timeoutId = setTimeout(() => {
+        fetch(`https://cds.unistra.fr/cgi-bin/nph-sesame/-oIx/~S?${name}`)
+            .then(response => response.text())
             .then(xmlString => {
+                // Rest of your code...
                 const parser = new DOMParser();
                 const xmlDoc = parser.parseFromString(xmlString, "text/xml");
                 const resolver = xmlDoc.querySelector("Resolver");
@@ -72,6 +82,129 @@ function checkName() {
                     }
                 }
                 runPopovers();
-            })
+            // runPopovers();
+            });
+    }, 500); // Delay set to 0.5 seconds (500 milliseconds)
     runPopovers();
-}
+  }
+
+
+// function checkName() {
+//     const name = nameInput.value;
+//     popover = bootstrap.Popover.getInstance(searchNameInput);
+//     if (popover) {
+//         popover.dispose()
+//     } 
+//     if (searchNameInput.value.length !== 0) {
+//         searchNameInput.setAttribute('data-bs-toggle', 'popover');
+//         searchNameInput.setAttribute('data-bs-trigger', 'manual');
+//         searchNameInput.setAttribute('data-bs-placement', 'top');
+//         searchNameInput.setAttribute('data-bs-content', 'Loading...')
+//     } else {
+//         popover.dispose();
+//     }
+
+//     // Clear the previous timeout
+//     clearTimeout(timeoutId);
+
+//     // Set a new timeout to execute the function after 2 seconds of inactivity
+//     timeoutId = setTimeout(() => {
+//         fetch(`https://cds.unistra.fr/cgi-bin/nph-sesame/-oIx/~S?${name}`)
+//             .then(response => response.text())
+//             .then(xmlString => {
+//                 // Rest of your code...
+//                 const parser = new DOMParser();
+//                 const xmlDoc = parser.parseFromString(xmlString, "text/xml");
+//                 const resolver = xmlDoc.querySelector("Resolver");
+//                 if (resolver) {
+//                     console.log(resolver);
+//                     searchBtn.removeAttribute('disabled');
+//                     searchNameInput.classList.remove('not-found');
+//                     searchNameInput.classList.add('found');
+//                     searchNameInput.style.color = 'seagreen';
+
+//                     popover = bootstrap.Popover.getInstance(searchNameInput);
+//                     if (popover) {
+//                         popover.dispose()
+//                     } 
+//                     if (searchNameInput.value.length !== 0) {
+//                         searchNameInput.setAttribute('data-bs-toggle', 'popover');
+//                         searchNameInput.setAttribute('data-bs-trigger', 'manual');
+//                         searchNameInput.setAttribute('data-bs-placement', 'top');
+//                         searchNameInput.setAttribute('data-bs-content', `${searchNameInput.value} resolved by SESAME (SIMBAD).`)
+//                     } else {
+//                         popover.dispose();
+//                     }
+//                 } else {
+//                     searchBtn.setAttribute('disabled', '');
+//                     searchNameInput.classList.remove('found');
+//                     searchNameInput.classList.add('not-found');
+//                     searchNameInput.style.color = 'tomato';
+
+//                     popover = bootstrap.Popover.getInstance(searchNameInput);
+//                     if (popover) {
+//                         popover.dispose();
+//                     } 
+//                     if (searchNameInput.value.length !== 0) {
+//                         searchNameInput.setAttribute('data-bs-toggle', 'popover');
+//                         searchNameInput.setAttribute('data-bs-trigger', 'manual');
+//                         searchNameInput.setAttribute('data-bs-placement', 'top');
+//                         searchNameInput.setAttribute('data-bs-content', `${searchNameInput.value} not resolved.`);
+//                     } else {
+//                         popover.dispose();
+//                     }
+//                 }
+//                 runPopovers();
+//             });
+//         runPopovers();
+//         }, 2000); // Delay set to 2 seconds (2000 milliseconds)
+//     }
+
+//     fetch(`https://cds.unistra.fr/cgi-bin/nph-sesame/-oIx/~S?${name}`)
+//         .then(response => response.text())
+//             .then(xmlString => {
+//                 const parser = new DOMParser();
+//                 const xmlDoc = parser.parseFromString(xmlString, "text/xml");
+//                 const resolver = xmlDoc.querySelector("Resolver");
+//                 if (resolver) {
+//                     console.log(resolver);
+//                     searchBtn.removeAttribute('disabled');
+//                     searchNameInput.classList.remove('not-found');
+//                     searchNameInput.classList.add('found');
+//                     searchNameInput.style.color = 'seagreen';
+
+//                     popover = bootstrap.Popover.getInstance(searchNameInput);
+//                     if (popover) {
+//                         popover.dispose()
+//                     } 
+//                     if (searchNameInput.value.length !== 0) {
+//                         searchNameInput.setAttribute('data-bs-toggle', 'popover');
+//                         searchNameInput.setAttribute('data-bs-trigger', 'manual');
+//                         searchNameInput.setAttribute('data-bs-placement', 'top');
+//                         searchNameInput.setAttribute('data-bs-content', `${searchNameInput.value} resolved by SESAME (SIMBAD).`)
+//                     } else {
+//                         popover.dispose();
+//                     }
+//                 } else {
+//                     searchBtn.setAttribute('disabled', '');
+//                     searchNameInput.classList.remove('found');
+//                     searchNameInput.classList.add('not-found');
+//                     searchNameInput.style.color = 'tomato';
+
+//                     popover = bootstrap.Popover.getInstance(searchNameInput);
+//                     if (popover) {
+//                         popover.dispose();
+//                     } 
+//                     if (searchNameInput.value.length !== 0) {
+//                         searchNameInput.setAttribute('data-bs-toggle', 'popover');
+//                         searchNameInput.setAttribute('data-bs-trigger', 'manual');
+//                         searchNameInput.setAttribute('data-bs-placement', 'top');
+//                         searchNameInput.setAttribute('data-bs-content', `${searchNameInput.value} not resolved.`);
+//                     } else {
+//                         popover.dispose();
+//                     }
+//                 }
+//                 runPopovers();
+//             })
+//     runPopovers();
+// }
